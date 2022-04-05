ec766f94bf0dc383b5fa4ef64ece1d21c21a8bde
Frank Nguyen <frankknguyen@gmail.com>
2022-04-04 23:44:37 -0500

    Added @mui/material-icons package. Moved functionality of map to MapContainer

--- client/Components/MapContainer.tsx
+++ client/Components/MapContainer.tsx
@@ -3,15 +3,22 @@ import LocationMap from './LocationMap';
 import { Container, Grid } from '@mui/material';
 import LocationButton from './LocationButton'
 import PinTypeButton from './PinTypeButton'
+import { Map, Marker } from "pigeon-maps"
 
 
 const MapContainer = () => {
+  const markers = [<Marker width={50} anchor={[40.7406, -73.9940]} onClick={() => alert('Market clicked')} />, <Marker width={50} anchor={[40.7506, -73.9900]} />, <Marker width={50} anchor={[40.7506, -73.9935]} />];
 
   return (
     <Container>
-      <LocationButton />
-      <PinTypeButton />
-      <LocationMap />
+      <Grid container direction='row'>
+        <LocationButton />
+        <PinTypeButton />
+      </Grid>
+      {/* <LocationMap /> */}
+      <Map height={600} defaultCenter={[40.7831, -73.9712]} defaultZoom={11}>
+        {markers}
+      </Map>
     </Container>
   )
 }
