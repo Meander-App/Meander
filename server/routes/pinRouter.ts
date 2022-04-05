b5a75bf9ec4b6e830cc1e1a8c0f6c6d4df755fe1
Anthony Guan <guananthony@gmail.com>
2022-04-05 09:35:57 -0700

    Fix responses for up- and down-votes

--- server/routes/pinRouter.ts
+++ server/routes/pinRouter.ts
@@ -14,19 +14,11 @@ pinRouter.post('/', pinController.addPin, (req, res) => {
 
 // need to add /upVote and /downVote because routes are the same otherwise
 pinRouter.patch('/upVote/:id', pinController.upVote, (req, res) => {
-	res
-		.status(200)
-		.send(
-			`${res.locals.upVotedPin.name} pin now has ${res.locals.upVotedPin.votes} votes.`
-		);
+	res.status(200).send(res.locals.upVotedPin.votes);
 });
 
 pinRouter.patch('/downVote/:id', pinController.downVote, (req, res) => {
-	res
-		.status(200)
-		.send(
-			`${res.locals.downVotedPin.name} pin now has ${res.locals.downVotedPin.votes} votes.`
-		);
+	res.status(200).send(res.locals.downVotedPin.votes);
 });
 
 // save for later
