8b13c7b1c0970f353c8f41664e5e18ddbb0f4b37
Paul Perez <pau.per92@gmail.com>
2022-04-06 16:50:29 -0400

    updating modal open state

--- client/Components/EventCards.tsx
+++ client/Components/EventCards.tsx
@@ -57,24 +57,7 @@ const EventCards = (props: any) => {
 					Comments
 				</Button>
 				{/* {props.open && <CommentModal />} */}
-				<div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
+				
 				<Button size='small' color='primary'>
 					{votes} votes
 				</Button>
